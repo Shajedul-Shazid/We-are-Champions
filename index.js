94c8116124cf3d12b0786b14fcf7d45920575f5f
Shajedul-Shazid <programmershajid@gmail.com>
2024-01-15 20:14:47 +0600

    data-comes-from-firebase-database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://endrosements-5a40c-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -9,17 +9,23 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const endrosementInDB = ref(database, "endrosement")
 
-
 const publishBtn = document.getElementById("pubish-btn")
 const textareaEl = document.getElementById("textarea-el")
-const formInputEl = document.getElementById("form-input-el")
-const formToEl = document.getElementById("form-to-el")
+const endorsementEl = document.getElementById("indorsement")
 
 publishBtn.addEventListener("click", function() {
     let textareavalue = textareaEl.value
     push(endrosementInDB, textareavalue)
     textareaEl.value = ""
-    formInputEl.value = ""
-    formToEl.value = ""
 })
 
+onValue(endrosementInDB, function(snapshot) {
+
+        let endrosementArray = Object.values(snapshot.val()) 
+
+        for(let i = 0; i < endrosementArray.length; i++) {          
+           let currentEndrosement = endrosementArray[i]
+            endorsementEl.innerHTML += `<li>${currentEndrosement}</li>`
+        }
+
+})
